d06e4f871b4702d3e28e26d93e99a5f81ee6e6ea
ahmafi <ahmafi@users.noreply.github.com>
2022-07-27 09:53:37 +0000

    deploy: 88c1e10cb10c936c03918d786ed993c13d4aa304

---
+++ _next/static/mIuEuXdWa_GOe1We7lNes/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-036d6f9e58930c1a.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
