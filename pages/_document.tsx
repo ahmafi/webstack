1bd5e786ab768a5043c881c1162900eb1c4657f8
Amir Hossein Mafi <amir77mafi@gmail.com>
2022-07-28 18:19:37 +0430

    Set HTML tag lang and dir attributes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="fa-IR" dir="rtl">
       <Head>
         <link
           rel="apple-touch-icon"
